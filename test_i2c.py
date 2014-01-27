b7c60749b8774f68d958304b129fd1948e3a4562
Sagar Pandya <sagargp@gmail.com>
2014-01-26 21:33:09 -0700

    Local changes trying to get i2c to work

--- test_i2c.py
+++ test_i2c.py
@@ -1,12 +1,48 @@
 import smbus
+import struct
+import time
+
+def get_motor_packet(left_speed, right_speed):
+  """
+  Speeds should be between -255 and 255
+  """
+  if left_speed < 0:
+    left_mode = 2
+    left_command = -left_speed
+  elif left_speed > 0:
+    left_mode = 0
+    left_command = left_speed
+  else:
+    left_mode = 1
+    left_command = 0
+
+  if right_speed < 0:
+    right_mode = 2
+    right_command = -right_speed
+  elif right_speed > 0:
+    right_mode = 0
+    right_command = right_speed
+  else:
+    right_mode = 1
+    right_command = 0
+
+  return struct.pack('BBBBBB', ord('H'), ord('B'), left_mode, left_command, right_mode, right_command)
 
 bus = smbus.SMBus(1)
 address = 0X04
 
-bus.write_byte(address, 'V')
-bus.write_byte(address, 'O')
+bus.write_byte(address, ord('V'))
+bus.write_byte(address, ord('O'))
 
 response_0 = bus.read_byte(address)
 response_1 = bus.read_byte(address)
 
-print ord(response_0) * 256 + ord(response_1)
+print response_0 * 256 + response_1
+
+for byte in get_motor_packet(100, 100):
+  bus.write_byte(address, ord(byte))
+
+time.sleep(2)
+
+for byte in get_motor_packet(0, 0):
+  bus.write_byte(address, ord(byte))
